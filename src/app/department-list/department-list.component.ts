6b9ad506895d39c396cd22e3b599c2b245e0fce2
manutyagi <manutyagi4@gmail.com>
2018-07-31 23:33:02 +0530

    routing demo 1 commit

---
+++ src/app/department-list/department-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute,ParamMap} from '@angular/router';
+import { relative } from 'path';
+
+@Component({
+  selector: 'app-department-list',
+  template: `
+    <h3>
+      Department List
+    </h3>
+    <ul class="items">
+      <li (click)="onSelect(department)" [class.selected]="isSelected(department)" *ngFor="let department of departments">
+        <span class="badge">{{department.id}}</span>{{department.name}}
+      </li>
+    </ul>
+  `,
+  styles: []
+})
+export class DepartmentListComponent implements OnInit {
+
+  public selectedId;
+  departments = [
+    {"id": 1, "name": "Angular"},
+    {"id": 2, "name": "Node"},
+    {"id": 3, "name": "MongoDB"},
+    {"id": 4, "name": "Ruby"},
+    {"id": 5, "name": "Bootstrap"}
+  ]
+  constructor(private router:Router,private route:ActivatedRoute) { }
+
+  ngOnInit() {
+
+    this.route.paramMap.subscribe((params: ParamMap) =>{
+      let id =parseInt(params.get('id'));
+      this.selectedId = id;
+    });
+  }
+  onSelect(department){
+    //this.router.navigate(['/departments',department.id])  -->this is called Absolute Navigation
+    this.router.navigate([department.id],{relativeTo:this.route}); //Relative Navigation: This means that whatever be the URL now,but to the current route 
+                                                                    //append the department id      
+  }
+  isSelected(department){
+    return department.id == this.selectedId;
+  }
+}
